a69414abd416938550997bff45b945d5e86ae829
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-27 23:25:17 -0800

    add calculations to JS logic to determine when the next arrival is and the minutes away based on the current time and the inputted frequency - this only works if user enters train start time perfectly; working on that part

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,11 +12,6 @@ firebase.initializeApp(config);
 // Assign variable to point to Firebase database
 var database = firebase.database();
 
-// Assign variable to hold current time for comparison to train time and frequency to calculate next arrival time and minutes away
-var currentTime = moment().format('LTS');
-// Console log for testing - can't just assign value and have it update continuously
-console.log(currentTime);
-
 // Add event listener for submit button to begin function for adding new train lines
 $("#submit-button").on("click", function (event) {
     // Prevent default action of submitting a form, which is refresh the page
@@ -26,7 +21,7 @@ $("#submit-button").on("click", function (event) {
     var trainName = $("#train-name").val().trim();
     var trainDestination = $("#destination").val().trim();
     // Add moment.js format for military time and add function to check if it's the correct format when inputted
-    var trainTime = moment.unix($("#train-time").val().trim()).format("HH:mm");
+    var trainTime = $("#train-time").val().trim();
     var trainFrequency = $("#frequency").val().trim();
     trainFrequency = parseInt(trainFrequency);
 
@@ -57,8 +52,6 @@ $("#submit-button").on("click", function (event) {
         console.log(newTrain.start);
         console.log(newTrain.frequency);
 
-        // alert("Employee successfully added"); Research modals to inform user that the train was added successfully
-
         // Clear all of the input text fields to make ready for another entry
         $("#train-name").val("");
         $("#destination").val("");
@@ -77,27 +70,62 @@ database.ref().on("child_added", function (childSnapshot) {
     var trainTime = childSnapshot.val().start;
     var trainFrequency = childSnapshot.val().frequency;
 
+    // Add moment.js or simple JS method to format start time to military time
+    // var trainStartTime = moment.unix(trainTime).format("HH:mm");
+
+    // Calculate the difference between trainStartTime and current time for 'Next Arrival' and 'Minutes Away'
+
+    // Assign variable to hold moment object of first train time
+    var firstTrainTime = moment(trainTime, "HH:mm");
+    // Assign variable to hold current time for comparison
+    var currentTime = moment();
+
+    // Calculate time difference between currentTime and firstTrainTime
+    var timeDifference = currentTime.diff(firstTrainTime, 'minutes');
+
+    // Assign minutesAway and nextTrainTime to reassign in conditionals
+    var minutesAway = 0;
+    var nextTrainTime = moment();
+
+    // Add condition to check if the time difference is less than or equal to 0 to determine whether the first train has arrived
+    if (timeDifference <= 0) {
+        // Reassign minutesAway to the time that remains before the arrival of the first train
+        minutesAway = -timeDifference;
+        // Reassign the nextTrainTime to the first scheduled train since they are referencing the same train
+        nextTrainTime = firstTrainTime;
+    }
+    // Else condition where the first train has arrived
+    else {
+        // Calculate the time since the last train as the remainder of the timeDifference and the trainFrequency
+        var timeSinceLastTrain = (timeDifference % trainFrequency);
+        // Subtract that from trainFrequency to determine how many minutes until the next train
+        minutesAway = (trainFrequency - timeSinceLastTrain);
+        // Reassign nextTrainTime to the currentTime plus the minutesAway
+        nextTrainTime = currentTime.add(minutesAway, 'minutes');
+    }
+
     // Console log for testing
     console.log(trainName);
     console.log(trainDestination);
     console.log(trainTime);
     console.log(trainFrequency);
-
-    // Add moment.js or simple JS method to format start time to military time
-    var trainStartTime = moment.unix(trainTime).format("HH:mm");
-
-    // Calculate the difference between trainStartTime and current time for 'Next Arrival' and 'Minutes Away'
-
+    console.log(timeDifference);
+    console.log(minutesAway);
+    console.log(nextTrainTime);
 
     // Assign a variable to ynamically create a new row
     var newTrainRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDestination),
         $("<td>").text(trainFrequency),
-        $("<td>").text(trainStartTime), // Switch to nextArrival when calculation is complete
-        $("<td>").text("Not Available") // Switch to minutesAway when calculation is complete
+        $("<td>").text(nextTrainTime.format("HH:mm")),
+        $("<td>").text(minutesAway)
     );
 
     // Append the new row to the table
     $("#train-table > tbody").append(newTrainRow);
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
 });
