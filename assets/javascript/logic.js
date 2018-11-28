a000cf0544faf64f2c810c2f394a1f778e804f34
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-27 18:58:05 -0800

    link Firebase database in HTML; initialize Firebase in JavaScript

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAKKsUI20X6x_ov5-dSXjvkk0JxzXWv4WQ",
+    authDomain: "train-scheduler-77a7d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-77a7d.firebaseio.com",
+    projectId: "train-scheduler-77a7d",
+    storageBucket: "train-scheduler-77a7d.appspot.com",
+    messagingSenderId: "386972897169"
+  };
+  firebase.initializeApp(config);
+
+  
